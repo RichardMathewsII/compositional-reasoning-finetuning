297eb4a5d85d7f2578fb2a40fe8245a50f363bff
RichardMathewsII <richardmathews.ai@gmail.com>
2023-07-07 16:15:13 -0500

    finish implementing functions

--- evaluation.py
+++ evaluation.py
@@ -34,12 +34,15 @@ Stage 2: Assess responses
 """
 
 from dataclasses import dataclass
+from difflib import SequenceMatcher
 from typing import Any, Dict, List, Tuple, Iterable
 from training_utils import qa_split
 import json
 from data_loaders import load_TestData
 from tqdm import tqdm
-from transformers import TFT5ForConditionalGeneration, T5Tokenizer
+from thefuzz import fuzz
+import argparse
+# from transformers import TFT5ForConditionalGeneration, T5Tokenizer
 
 
 @dataclass
@@ -48,10 +51,12 @@ class EvaluationConfig(object):
     model: str
     dataset: str
     path: str
+    create_tokenizer: bool = True
     
     def __post_init__(self):
-        if "t5" in self.model:
-            self._set_t5_tokenizer()
+        if self.create_tokenizer:
+            if "t5" in self.model:
+                self._set_t5_tokenizer()
     
     def _set_t5_tokenizer(self):
         tokenizer = T5Tokenizer.from_pretrained("t5-base")
@@ -129,27 +134,45 @@ def decode(tokens: Iterable, config: EvaluationConfig) -> Iterable[str]:
 
 def extract_answer(generated_text: str) -> str:
     '''Extracts the answer from a generated text.'''
+    # extract last line
     if '\n' not in generated_text:
+        # e.g. 'So the final answer is: <answer>'
+        # e.g. '<answer>'
         last_line =  generated_text
-    else: 
-        last_line = generated_text.split('\n')[-1]
-
-    if ':' not in last_line:
-        after_colon = last_line
     else:
-        after_colon = generated_text.split(':')[-1]
-    
-    if ' ' == after_colon[0]:
-        after_colon = after_colon[1:]
-    if '.' == after_colon[-1]:
-        after_colon = after_colon[:-1]
+        pieces = generated_text.split('\n')
+        if pieces[-1] == '':
+            # e.g. 'So the final answer is: <answer>\n' -> 'So the final answer is: <answer>'
+            last_line = pieces[-2]
+        else:
+            # e.g. '...Intermediate answer: ...\nSo the final answer is: <answer>'
+            # -> 'So the final answer is: <answer>'
+            last_line = pieces[-1]
 
-    return after_colon
+    # extract text after colon
+    if ':' not in last_line:
+        # e.g. '<answer>'
+        answer = last_line
+    else:
+        # e.g. 'So the final answer is: <answer>' -> '<answer>'
+        answer = last_line.split(':')[-1]
+
+    # remove leading and trailing whitespace
+    answer = answer.strip()
+    return answer
 
 
 def check_self_ask(generated_text: str) -> bool:
     '''Checks if self-ask rationale is used.'''
-    pass
+    ngrams = ["intermediate answer", "follow up"]
+    matches = 0
+    for ngram in ngrams:
+        if ngram in generated_text.lower():
+            matches += 1
+    if matches > 0:
+        return True
+    else:
+        return False
 
 
 def store_responses(response: List[Dict[str, Any]], config: EvaluationConfig) -> None:
@@ -177,7 +200,12 @@ def check_correct(generated_answer: str, true_answer: str) -> int:
     -------
     0 if wrong, 1 if correct
     '''
-    pass
+    # use similarity, strings may not be exact match
+    score = fuzz.partial_ratio(generated_answer.strip().lower(), true_answer.strip().lower())  
+    if score > 95:
+        return 1
+    else:
+        return 0
 
 
 def store_evaluation_results(results: Dict, config: EvaluationConfig) -> None:
@@ -194,12 +222,25 @@ if __name__ == "__main__":
     path = "data/MultihopEvaluation/"
 
     # Stage 0: Setup
-    MODEL = 0
-    DATASET = 0
-    SIZE = -1  # TODO: fill in size of dataset
-    BATCH_SIZE = 1000
+    parser = argparse.ArgumentParser(description='Run llm QA evaluation.')
+    parser.add_argument('--model', help=f'Language model id, one of {model_options}', type=str)
+    parser.add_argument('--dataset', help=f'Dataset type, one of {dataset_options}', type=str)
+    parser.add_argument('--size', help='Size of dataset to evaluate on', type=int)
+    parser.add_argument('--batch_size', help='Batch size', type=int)
 
-    config = EvaluationConfig(model_options[MODEL], dataset_options[DATASET], path)
+    args = parser.parse_args()
+
+    if args.model not in model_options:
+        raise ValueError(f"Model must be one of {model_options}")
+    if args.dataset not in dataset_options:
+        raise ValueError(f"Dataset must be one of {dataset_options}")
+
+    MODEL = "t5-3b" if args.model is None else args.model
+    DATASET = "direct" if args.dataset is None else args.dataset
+    SIZE = 12576 if args.size is None else args.size  # full size
+    BATCH_SIZE = 100 if args.batch_size is None else args.batch_size
+
+    config = EvaluationConfig(MODEL, DATASET, path)
 
     # Stage 1: Collect responses
     model = load_model(config)
